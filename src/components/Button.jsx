a64fb426d5a533e822dda1da93b6cbfd56294dd5
Jen Thomas James <jenthomasjames@gmail.com>
2024-03-03 21:26:40 +0530

    Comp: Add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+
+const Button = ({ children, variant = "contained" }) => {
+  return (
+    <button
+      className={clsx("px-3 py-1 rounded-md transition-all", {
+        "bg-primary-500 text-primary-50 hover:bg-primary-700":
+          variant === "contained",
+        "bg-transparent border border-primary-500 text-primary-500 hover:bg-primary-50":
+          variant === "outlined",
+        "text-primary-500 bg-transparent hover:bg-primary-50":
+          variant === "text",
+      })}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
