287000f75ba8a9b28315aeb945abdd61cc970c28
Jen Thomas James <jenthomasjames@gmail.com>
2024-03-03 20:56:18 +0530

    dep: Add tailwindcss to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 const App = () => {
-  return "Fakestore UI works";
+  return <h1 className="text-3xl font-bold underline text-blue-300">Fakestore UI works</h1>;
 };
 
 export default App;
